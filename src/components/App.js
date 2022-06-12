0d5a6389142680d529417d434d62a817fae9c2dd
Shivangi Rajput <90110930+shivangirajput123@users.noreply.github.com>
2022-06-12 22:50:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,50 @@
-import React, { useState } from “react”;
-import “../styles/App.css”;
-import { signUpFormValidation } from “../utils/validation”;
+import React, { useState } from "react";
+import "../styles/App.css";
+import { signUpFormValidation } from "../utils/validation";
+
 const App = () => {
-  const [error, setError] = useState({ name: “”, email: “”, password: “” });
+  const [error, setError] = useState({ name: "", email: "", password: "" });
+
   const validate = (event) => {
     event.preventDefault();
+
     const errors = signUpFormValidation({
       email: event.target.email.value,
       password: event.target.password.value,
       name: event.target.name.value,
     });
     if (errors == null) {
-      setError({ name: “”, email: “”, password: “” });
+      setError({ name: "", email: "", password: "" });
     } else {
       setError(errors);
     }
   };
+
   return (
     <>
       <form onSubmit={validate}>
         <div>
           <label>
             Name :
-            <input id=“name” type=“text” name=“name” />
+            <input id="name" type="text" name="name" />
           </label>
-          {error.name != null && error.name != “” ? error.name : null}
+          {error.name != null && error.name != "" ? error.name : null}
         </div>
         <br />
         <div>
           <label>
             Email :
-            <input id=“email” type=“text” name=“email” />
+            <input id="email" type="text" name="email" />
           </label>
-          {error.email != null && error.email != “” ? error.email : null}
+          {error.email != null && error.email != "" ? error.email : null}
         </div>
         <br />
         <div>
           <label>
             Password :
-            <input id=“password” type=“text” name=“password” />
+            <input id="password" type="text" name="password" />
           </label>
-          {error.password != null && error.password != “”
+          {error.password != null && error.password != ""
             ? error.password
             : null}
         </div>
@@ -48,15 +52,16 @@ const App = () => {
         <div>
           <label>
             Consent :
-            <input id=“consent” type=“checkbox” name=“consent” />
+            <input id="consent" type="checkbox" name="consent" />
           </label>
         </div>
         <br />
         <div>
-          <button type=“submit”>Signup</button>
+          <button type="submit">Signup</button>
         </div>
       </form>
     </>
   );
 };
+
 export default App;
